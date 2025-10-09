cf7da2240e315197f057df86a31633fe06390cd3
d0t <d0td0t@tuta.io>
2025-10-09 10:50:43 +0200

    add vgg16 based unet model

--- Training/model_unet.py
+++ Training/model_unet.py
@@ -2,6 +2,7 @@
 from keras.layers import Input, Conv2D, MaxPooling2D, concatenate, Conv2DTranspose, BatchNormalization, Activation, Dropout, Reshape, Permute
 # from tensorflow.python.keras.optimizers import Adadelta, Nadam
 from keras.models import Model
+from keras.applications import VGG16
 # from tensorflow.python.keras.utils import multi_gpu_model, plot_model
 
 
@@ -52,3 +53,49 @@ def Unet(img_height, img_width, nclasses=3, filters=64):
     
     model = Model(inputs=input_layer, outputs=output_layer, name='Unet')
     return model    
+
+def VGG16_Unet(img_height, img_width, nclasses=3):
+    # Input
+    input_layer = Input(shape=(img_height, img_width, 3), name='image_input')
+    
+    # Load pre-trained VGG16 as encoder
+    vgg16 = VGG16(weights='imagenet', include_top=False, input_tensor=input_layer)
+    
+    # Make encoder layers non-trainable
+    for layer in vgg16.layers:
+        layer.trainable = False
+    
+    # Get skip connections from VGG16
+    # VGG16 has 5 blocks with the following output names:
+    skip1 = vgg16.get_layer('block1_conv2').output  # 64 filters
+    skip2 = vgg16.get_layer('block2_conv2').output  # 128 filters  
+    skip3 = vgg16.get_layer('block3_conv3').output  # 256 filters
+    skip4 = vgg16.get_layer('block4_conv3').output  # 512 filters
+    
+    # Bottom of U-Net (last VGG16 block)
+    bottom = vgg16.get_layer('block5_conv3').output  # 512 filters
+    bottom = Dropout(0.5)(bottom)
+    
+    # Decoder (ascending part) - Trainable
+    # Up sampling block 1
+    deconv6 = deconv_block(bottom, residual=skip4, nfilters=512)
+    deconv6 = Dropout(0.5)(deconv6)
+    
+    # Up sampling block 2
+    deconv7 = deconv_block(deconv6, residual=skip3, nfilters=256)
+    deconv7 = Dropout(0.5)(deconv7)
+    
+    # Up sampling block 3
+    deconv8 = deconv_block(deconv7, residual=skip2, nfilters=128)
+    
+    # Up sampling block 4
+    deconv9 = deconv_block(deconv8, residual=skip1, nfilters=64)
+    
+    # Output layer
+    output_layer = Conv2D(filters=nclasses, kernel_size=(1, 1))(deconv9)
+    output_layer = BatchNormalization()(output_layer)
+    output_layer = Reshape((img_height*img_width, nclasses), input_shape=(img_height, img_width, nclasses))(output_layer)
+    output_layer = Activation('softmax')(output_layer)
+    
+    model = Model(inputs=input_layer, outputs=output_layer, name='VGG16_Unet')
+    return model
